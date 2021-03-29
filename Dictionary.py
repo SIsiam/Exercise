2c27e2007a4710ac96994f59f3c96dbaa67672ca
Siam <shahidulmd392@gmail.com>
2021-03-29 12:26:58 +0600

    exercise finished

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+
+Dictionary = {"ignore":"refuse to take notice of or acknowledge", "abandon":"cease to support or look after",
+        "exaggerate":"enlarged or altered beyond normal proportions", "prejudice":"preconceived opinion that is not based on reason or actual experience"}
+print("Enter the Word")
+Data1 = input("spam")
+print(Data1, "means", Dictionary[Data1])
